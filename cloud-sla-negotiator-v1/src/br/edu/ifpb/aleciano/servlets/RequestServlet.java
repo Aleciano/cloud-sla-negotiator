0cbf7cad0b956c5572c44d0470fc96e8c1020355
aleciano <aleciano@ieee.org>
2013-10-10 16:01:13 -0300

    Funcionando JSP com perguntas sobre conflitos, e JSP de resultados de estratégias.

--- cloud-sla-negotiator-v1/src/br/edu/ifpb/aleciano/servlets/RequestServlet.java
+++ cloud-sla-negotiator-v1/src/br/edu/ifpb/aleciano/servlets/RequestServlet.java
@@ -3,11 +3,13 @@ package br.edu.ifpb.aleciano.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
@@ -35,6 +37,7 @@ import com.google.gson.Gson;
 public class RequestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	ServletContext cx;
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -58,60 +61,117 @@ public class RequestServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		this.cx = request.getSession().getServletContext();
-		out.println("<!DOCTYPE HTML><html lang=\"pt-br\"><head><meta charset=\"ISO-8859-1\"><title>Novo registro: resultado</title></head><body>");
+		String flag = request.getParameter("flag");
+		switch (flag) {
+		case "login": {
+			HttpSession session = request.getSession();
+			/*
+			 * out.println(
+			 * "<!DOCTYPE HTML><html lang=\"pt-br\"><head><meta charset=\"ISO-8859-1\"><title>Novo registro: resultado</title></head><body>"
+			 * );
+			 * 
+			 * out.println("<b>Resultados:</b><br><br>");
+			 */
 
-		out.println("<b>Resultados:</b><br><br>");
-
-		Context context = createContextByArgs(request);
-		try {
-			// load up the knowledge base
-			KnowledgeBase kbase = readKnowledgeBase();
-			
-			StatefulKnowledgeSession ksession = kbase
-					.newStatefulKnowledgeSession();
-			
-			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory
-					.newFileLogger(ksession, "test");
-			// go !
+			/*
+			 * Cria uma engine e adiciona como atributo na sessão. SEMPRE.
+			 */
 			EngineReq engine = new EngineReq();
+			session.setAttribute("engine", engine);
 
-			ksession.insert(context);
-			ksession.insert(engine);
-			ksession.fireAllRules(); // permanece executando enquanto as regras
-										// estão trabalhando..
-			// System.out.println("Após o fire das regras..");
-			logger.close();
+			Context context = createContextByArgs(request);
+			try {
+				// load up the knowledge base
+				KnowledgeBase kbase = readKnowledgeBase();
 
-			if (engine.getStrategies().size() > 0)
-				for (Strategy str : engine.getStrategies()) {
-					out.println("\nEstratégia " + str.getName()
-							+ " é aplicável.");
+				StatefulKnowledgeSession ksession = kbase
+						.newStatefulKnowledgeSession();
+
+				KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory
+						.newFileLogger(ksession, "test");
+				// go !
+
+				ksession.insert(context);
+				ksession.insert(engine);
+				ksession.fireAllRules(); // permanece executando enquanto as
+											// regras
+											// estão trabalhando..
+				// System.out.println("Após o fire das regras..");
+				logger.close();
+
+				if (engine.getStrategies().size() > 0) {
+					// for (Strategy str : engine.getStrategies()) {
+					// TODO add array de estratégias e passa pro JSP de
+					// seleção
+
+					/*
+					 * out.println("\nEstratégia " + str.getName() +
+					 * " é aplicável.");
+					 */
+					// }
+
+					request.setAttribute("conflict", engine.getStrategies()
+							.size() > 1 ? "yes" : "no");
+					engine.setLogin(request.getParameter("login"));
+					engine.setKey(request.getParameter("key").toCharArray());
+					request.setAttribute("strategies", engine.getStrategies());
+					session.setAttribute("engine", engine);
+					// request.setAttribute("code", 1);
+					RequestDispatcher r = request
+							.getRequestDispatcher("WEB-INF/strategy_selection.jsp");
+					request.setCharacterEncoding("UTF-8");
+					r.forward(request, response);
+
+				} else {
+					session.setAttribute("strategies", null);
+					request.setAttribute("strategy", null);
+					RequestDispatcher r = request
+							.getRequestDispatcher("WEB-INF/results.jsp");
+					request.setCharacterEncoding("UTF-8");
+					r.forward(request, response);
+
 				}
-			else
-				out
-						.println("\nNão houve estratégias ativadas com estas opções.");
-			engine.setLogin(request.getParameter("login"));
-			engine.setKey(request.getParameter("key").toCharArray());
-			if (engine.startRequest(0))
-				out.println("\nSucesso! "
-						+ engine.getStrategies().get(0).getName()
-						+ " adquiriu uma instância com sucesso.");
-			else
-				out.println("Não foi possível "
-						+ engine.getStrategies().get(0).getName()
-						+ " adquirir uma instância.");
-		} catch (Throwable t) {
-			t.printStackTrace();
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+			break;
 		}
 
 		/*
-		 * out.println(context.toString());
-		 * //System.out.println(context.toString());
-		 * 
-		 * out.println(request.getParameter("login"));
-		 * out.println(request.getParameter("key"));
+		 * Recebe 'yes' ou 'no'. Caso 'yes', lê o atributo da estratégia
+		 * ativada. Pode ser o index do Array ou o nome da estratégia.
 		 */
-		out.println("</body></html>");
+		// TODO
+		case "selection": {
+			HttpSession session = request.getSession();
+			EngineReq engine = (EngineReq) session.getAttribute("engine");
+			int index = Integer
+					.parseInt(request.getParameter("strategy_index"));
+			if (engine != null && engine.startRequest(index)) {
+				// TODO manda pro JSP de resultados a estratégia escolhida e
+				// ativada.
+				session.setAttribute("strategy",
+						engine.getStrategies().get(index));
+				request.setAttribute("strategy",
+						engine.getStrategies().get(index));
+				RequestDispatcher r = request
+						.getRequestDispatcher("WEB-INF/results.jsp");
+				request.setCharacterEncoding("UTF-8");
+				r.forward(request, response);
+			} else {
+				// TODO manda pro JSP de resultados falando que não houve
+				// sucesso ao utilizar a estratégia.
+				/*
+				 * out.println("Não foi possível " +
+				 * engine.getStrategies().get(0).getName() +
+				 * " adquirir uma instância.");
+				 */
+				engine.toString(); // nada
+			}
+		}
+			break;
+		}
+		// out.println("</body></html>");
 
 	}
 
@@ -160,17 +220,16 @@ public class RequestServlet extends HttpServlet {
 		return context;
 	}
 
-	private /*static*/ KnowledgeBase readKnowledgeBase() throws Exception {
-		
+	private/* static */KnowledgeBase readKnowledgeBase() throws Exception {
+
 		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
 				.newKnowledgeBuilder();
-		
+
 		/* Ler de um arquivo */
-		kbuilder.add(ResourceFactory
-				.newFileResource(cx.getRealPath("/") + /*"WEB-INF/" +*/"regras.drl"),
-				ResourceType.DRL);
-		
-		
+		kbuilder.add(
+				ResourceFactory.newFileResource(cx.getRealPath("/")
+						+ "WEB-INF/" + "regras.drl"), ResourceType.DRL);
+
 		KnowledgeBuilderErrors errors = kbuilder.getErrors();
 		if (errors.size() > 0) {
 			for (KnowledgeBuilderError error : errors) {
